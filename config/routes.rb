effcdaec88a0aab1428483ba43df14b906a1777a
Orson <orson@MacBook-Air.local>
2021-08-09 22:56:29 +0800

    Add routes to home and about us

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root 'pages#homepage'
+
+get "/home", to: "pages#homepage"
+get "/about_us", to: "pages#about_us"
 end
